e6821ca1f8a795f9cab8b1510674b18b795cce95
ryohei_wn <osakana.ryohei@gmail.com>
2015-11-30 12:50:13 +0900

    update base.vb

--- sience_calc/base.vb
+++ sience_calc/base.vb
@@ -2,19 +2,32 @@
 
 Module base
 
+    Dim 入力値 As String 
+    Dim 変換値 As Double  = CType(入力値,Double)
+    Dim 解 As String 
+
     Public Sub Main()
-       型変換
+       入力処理
     End Sub
     
     
-    Dim 入力値 As String 
-     Public Sub 型変換()
-        Dim 入力値 As Double = CType(入力値,Double)
+   Public Sub 入力処理()
+        
+        入力値 = Console.ReadLine()
+        Dim 変換値 As Double  = CType(入力値,Double)
         
     End Sub
 
-        Public Sub コンソール出力()
-            Dim 解 As Double 
+    Public Sub 演算処理
+       
+        Dim x As Double = 変換値
+        Dim z As Double 
+        z = x
+        解 = z
+    End Sub
+
+        Sub コンソール出力
+        
          Console.WriteLine(解)
     End Sub
 
